fb7b5d5b5c9ad4b475a1cf5ca9261073fd48fd6a
LayWasTaken <clarencetoledo08@gmail.com>
2024-07-06 18:07:42 +0800

    Registered PacketHooker

--- src/Lay/BossApi/Loader.php
+++ src/Lay/BossApi/Loader.php
@@ -2,6 +2,7 @@
 
 namespace Lay\BossApi;
 
+use CortexPE\Commando\PacketHooker;
 use Lay\BossApi\commands\SummonBoss;
 use Lay\BossApi\entity\examples\EvokerBoss;
 use pocketmine\plugin\PluginBase;
@@ -10,7 +11,9 @@ final class Loader extends PluginBase {
     
     public function onEnable(): void{
         BossRegistry::register("evoker_boss", EvokerBoss::class);
-
+        if(!PacketHooker::isRegistered()) {
+            PacketHooker::register($this);
+        }
         $this->getServer()->getCommandMap()->register("bossapi", new SummonBoss($this, "summonboss", "Summon a boss specified by the id"));
     }
 
